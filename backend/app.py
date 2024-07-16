48c3ccc4f3442558f1c8567492f6d63c4d4701aa
Ahmad Dimashkie <afd08@mail.aub.edu>
2024-07-16 20:56:21 +0300

    created api calls for every university in the DB

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,38 @@
 from flask import Flask
-from database.database_functions import get_uni_items
+from database.database_functions import get_unis_items, get_uni_info
 from flask import Flask, jsonify
 
 
 app = Flask(__name__)
 
 @app.route('/get-all-items')
-def get_uni_info():
-    data = get_uni_items()
+def get_all_unis():
+    data = get_unis_items()
+    return jsonify(data)
+
+@app.route('/get-NDU-info')
+def get_ndu_info():
+    data = get_uni_info(0)
+    return jsonify(data)
+
+@app.route('/get-AUB-info')
+def get_aub_info():
+    data = get_uni_info(1)
+    return jsonify(data)
+
+@app.route('/get-LAU-info')
+def get_lau_info():
+    data = get_uni_info(2)
+    return jsonify(data)
+
+@app.route('/get-BAU-info')
+def get_bau_info():
+    data = get_uni_info(3)
+    return jsonify(data)
+
+@app.route('/get-USJ-info')
+def get_usj_info():
+    data = get_uni_info(4)
     return jsonify(data)
 
 
--- backend/database/database_functions.py
+++ backend/database/database_functions.py
@@ -1,15 +1,32 @@
 import boto3
+import logging
 
 def connect_to_db():
     dynamo_client = boto3.client('dynamodb')
     return dynamo_client
 
-def get_uni_items():
+def get_unis_items():
     db = connect_to_db()
-    return db.scan(
-        TableName='UniFind'
-    )
-   
+    try:
+        return db.scan(
+            TableName='UniFind'
+        )
+    except Exception as e:
+        logging.error(f"Error scanning items from DynamoDB: {e}")
+        return None
 
+def get_uni_info(university_id):
+    db = connect_to_db()
+    try:
+        response = db.get_item(
+            TableName='UniFind',
+            Key={
+                'UniversityID': {'N':  str(university_id)}
+            }
+        )
+        return response.get('Item', None)
+    except Exception as e:
+        logging.error(f"Error getting item from DynamoDB: {e}")
+        return None
 
 
